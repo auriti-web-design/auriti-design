3dca6d4681c04861ca1177b3a8e0b41643835e37
auritiwdesign <juancamilo.auriti@gmail.com>
2024-03-14 05:06:24 +0100

    add custom class

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -24,9 +24,16 @@ const Hero = () => {
             <div className='container mx-auto'>
                 <div className='gap-x-8 flex justify-between'>
                     {/* text */}
-                    <div>Text</div>
+                    <div className='flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
+                        <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px] xl:text-left'>Frontend Developer</div>
+                        <h1 className='h1'>Hello, my name is Juan Camilo Auriti</h1>
+                        <p className='subtitle'>
+                            Brief description with insights into myself, my vocational journey, and what I engage in professionally.
+                        </p>
+                        {/* buttons */}
+                    </div>
                     {/* image */}
-                    <div>Image</div>
+                    <div className='xl:flex relative hidden'>Image</div>
                 </div>
                 <div
                 className='md:flex left-2/4 bottom-44 xl:bottom-12 animate-bounce absolute hidden'>
